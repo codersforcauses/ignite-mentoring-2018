a52b62cb8b407480c8cfbd1dc1ea3622d9018588
JKKim98 <j.k.kim98@outlook.com>
2019-01-04 01:11:53 +0800

    Corrected countClass().

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const minMentor = 5
 const maxMentor = 7
-let classcount = 0;
+let classCount = 0;
 let classes = {};
 
 // Reads the uploaded CSV file and parse the data into objects
@@ -15,6 +15,8 @@ function readFile() {
             console.log(results.data);
             countClass(results.data);
             countPrefPerClass(results.data);
+            console.log(classCount);
+            console.log(classes);
 		},
 		error: errorFn
     };
@@ -25,13 +27,12 @@ function readFile() {
 // Counts the number of classes and adds classes as properties to the object classes
 function countClass(data) {
     let row = data[0];
-    let patt = new RegExp("Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday\s\d"); //TODO this returns positive for the WWC check property
     
     Object.entries(row).forEach(
         function(keyvaluepair) {
-            if (patt.test(keyvaluepair[0])) {
-                console.log(keyvaluepair[0]);
-                classcount++;
+            if (/Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday/.test(keyvaluepair[0]) 
+                && /\d{1,2}am|\d{1,2}pm/g.test(keyvaluepair[0])) {
+                classCount++;
                 classes[keyvaluepair[0]] = 0;
             }
         }
@@ -42,7 +43,6 @@ function countClass(data) {
 function countPrefPerClass(data) {
     data.forEach(function(mentor) {
         Object.entries(classes).forEach(function(aClass) {
-            console.log(mentor[aClass[0]]);
             if (mentor[aClass[0]] != "") {
                 classes[aClass[0]]++;
             }
