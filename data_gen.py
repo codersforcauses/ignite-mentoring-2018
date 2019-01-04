aefe4d7e15f2998fef9895264a3c145a5bf2cea7
Daniel Jacobs <dj6450@gmail.com>
2019-01-04 13:51:43 +0800

    added documentation to data_gen.py

--- data_gen.py
+++ data_gen.py
@@ -1,10 +1,20 @@
+"""Python script to generate fake data with a unique ID and 17 classes.
+   Each 'mentor' will preference 4 out of the 17 classes (1,2,3,4).
+   
+   Run the script for BASH with python data_gen.py
+   
+   This will create a file named 'fakedata.csv' with your data.
+   To change the amount of data generated change the DATAROWS variable below."""
+
+
 import csv
 import random
 
+DATAROWS = 130
 with open("fakedata.csv", "wb") as csvfile:
     
     csvfile.write('id, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17\n')
-    for x in range (0, 130):
+    for id in range (0, DATAROWS):
         classes = [0] * 17
         y=1
         while y<5:
@@ -13,7 +23,7 @@ with open("fakedata.csv", "wb") as csvfile:
                 classes[prefClass] = y
                 y+=1
 
-        csvline = str(x)+","
+        csvline = str(id)+","
         for g in range(0,17):
             csvline+=str(classes[g])
             if(g==16):
