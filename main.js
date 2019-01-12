cbe09fb2394e786bf56d0413ee7a852c12a37edf
Daniel Jacobs <dj6450@gmail.com>
2019-01-12 16:06:43 +0800

    Basic brute force algorithm with check for drivers, experience, size, preference

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const minMentor = 5
 const maxMentor = 7
+const numExperiencedRequired = 2;
 let classCount = 0;
 let classes = {};
 
@@ -18,6 +19,7 @@ function readFile() {
             countExperiencedMentors(results.data);
             console.log(classCount);
             console.log(classes);
+            bruteForce(results.data);
         },
         error: errorFn,
         transform: function(value, header) {
@@ -76,4 +78,83 @@ function countExperiencedMentors(data) {
 // Handles errors in parsing CSV file
 function errorFn() {
 
+}
+
+function bruteForce(data) {
+    let conditionsSatisfied = false;
+
+    let iterationCounter = 0;
+    let classAllocations;
+    while(!conditionsSatisfied) {
+        classAllocations = classesPreferedMentors(data);
+        let sizeSatisfied = checkSizes(classAllocations);
+        let driversSatisfied = checkAtLeastOneDriver(classAllocations);
+        let experiencedSatisfied = checkExperienced(classAllocations);
+        conditionsSatisfied = (sizeSatisfied && driversSatisfied && experiencedSatisfied) ? true : false;
+        iterationCounter++;
+    }
+    console.log(`Requirements satisfied in ${iterationCounter} random iterations!`);
+    
+    let classKeys = Object.keys(classAllocations);
+    for(let key in classKeys) {
+        console.log(`${classKeys[key]}:\n`);
+        for(let i=0; i<classAllocations[classKeys[key]].length; i++){
+            console.log(classAllocations[classKeys[key]][i]["Name"]);
+        }
+    }
+}
+
+function checkExperienced(classAllocations) {
+    let classKeys = Object.keys(classAllocations);
+    for(let key in classKeys) {
+        let experiencedCount = 0;
+        for(let i=0; i<classAllocations[classKeys[key]].length; i++) {
+            if(classAllocations[classKeys[key]][i]["Have you mentored before?"]) experiencedCount++;
+        }
+        if(experiencedCount<2) return false;
+    }
+    return true;
+}
+
+//Iterates through each class and it's allocated mentors and returns true if every class has at least one driver
+function checkAtLeastOneDriver(classAllocations) {
+    let classKeys = Object.keys(classAllocations);
+    for(let key in classKeys) {
+        let classHasDriver = false;
+        for(let i=0; i<classAllocations[classKeys[key]].length; i++) {
+            if(classAllocations[classKeys[key]][i]["Can you drive to and from your class?"]) classHasDriver = true;
+        }
+        if(!classHasDriver) return false;
+    }
+    return true;
+}
+
+function checkSizes(classAllocations) {
+    let classKeys = Object.keys(classAllocations);
+    for(let key in classKeys) {
+        //Currently only checks if the minimum mentor num is satisfied
+        if(classAllocations[classKeys[key]].length < minMentor){
+            return false;
+        }
+    }
+    return true;
+}
+
+function classesPreferedMentors(data) {
+    let classAllocations = {};
+    let classKeys = Object.keys(classes);
+    for(let key in classKeys) {
+        classAllocations[classKeys[key]] = [];
+    }
+
+    data.forEach(function(mentor) {
+        let randPref = Math.floor(Math.random() * (4-1 + 1)) + 1;
+
+        Object.entries(classes).forEach(function(aClass) {
+            let thisKey = aClass[0];
+            if (mentor[aClass[0]] == randPref) classAllocations[thisKey].push(mentor);
+        });
+    })
+
+    return classAllocations;
 }
